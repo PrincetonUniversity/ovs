58c6c4bdbddf5b452aa651b1f19f4d55c8813b16
Muhammad Shahbaz <mshahbaz@cs.princeton.edu>
2015-12-02 04:58:17 -0500

    minor edits

--- include/p4/examples/l2_switch/l2_switch.sh
+++ include/p4/examples/l2_switch/l2_switch.sh
@@ -11,7 +11,7 @@ $DIR/ovs-ofctl --protocols=OpenFlow15 add-flow br0 "table=0,priority=32768,ether
 $DIR/ovs-ofctl --protocols=OpenFlow15 add-flow br0 "table=0,priority=0                                      actions=controller"
 
 # DMAC Table 1
-$DIR/ovs-ofctl --protocols=OpenFlow15 add-flow br0 "table=1,priority=32768,ethernet__dstAddr=0x101112131415 actions=set_field:1->reg0,resubmit(,2)"
+$DIR/ovs-ofctl --protocols=OpenFlow15 add-flow br0 "table=1,priority=32768,ethernet__dstAddr=0x101112131415 actions=set_field:2->reg0,resubmit(,2)"
 $DIR/ovs-ofctl --protocols=OpenFlow15 add-flow br0 "table=1,priority=32768,ethernet__dstAddr=0x101112131417 actions=set_field:5->reg0,resubmit(,2)"
 $DIR/ovs-ofctl --protocols=OpenFlow15 add-flow br0 "table=1,priority=32768,ethernet__dstAddr=0x101112131419 actions=set_field:6->reg0,resubmit(,2)"
 $DIR/ovs-ofctl --protocols=OpenFlow15 add-flow br0 "table=1,priority=0                                      actions=flood"
