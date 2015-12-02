fca1e9a0ac01e1f9a259de13f1062c1dbc544e64
mshahbaz <mshahbaz@cs.princeton.edu>
2015-12-02 03:48:11 -0600

    minor edits

--- setup-scripts/automake.mk
+++ setup-scripts/automake.mk
@@ -3,6 +3,6 @@ EXTRA_DIST += \
 	setup-scripts/helpers/setup-hugetlbfs.sh \
 	setup-scripts/helpers/setup-interfaces.sh \
 	setup-scripts/helpers/setup-vars-ovs-dpdk.sh \
-	setup-scripts/setup-ovs.sh
-	setup-scripts/utilities/setup-bridge.sh
+	setup-scripts/setup-ovs.sh \
+	setup-scripts/utilities/setup-bridge.sh \
 	setup-scripts/utilities/clean-bridge.sh
