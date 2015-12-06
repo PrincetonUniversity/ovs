c94a04dd2acce3d435d9eac15898a3ce8b446871
Muhammad Shahbaz <mshahbaz@cs.princeton.edu>
2015-12-06 01:48:33 -0500

    minor edits

--- vswitchd/profile.sh
+++ vswitchd/profile.sh
@@ -8,15 +8,18 @@ source ./helpers/setup-vars-ovs-dpdk.sh
 cd ..
 operf -g ./vswitchd/ovs-vswitchd --dpdk -c 0x1 -n 4 -- unix:$DB_SOCK --pidfile
 
-# Generate png
-opreport -cgf | gprof2dot -f oprofile | dot -Tpng -o opreport.png
+mkdir -p ./oprofile_data/opreport
+
+# Generate png with and without call-graph 
+opreport -cgf | gprof2dot -f oprofile | dot -Tpng -o ./oprofile_data/opreport/stats.png
 
 # Generate diff
-if [ -d "./oparchive-previous" ]; then
-	opreport --session-dir=`pwd`/oprofile_data/ -xlg ./vswitchd/ovs-vswitchd { archive:./oparchive-previous } { } > oparchive.stats
+if [ -d "./oprofile_data/oparchive/previous" ]; then
+	mkdir -p ./oprofile_data/oparchive/previous
+	opreport --session-dir=`pwd`/oprofile_data/ -xlg ./vswitchd/ovs-vswitchd { archive:./oprofile_data/oparchive/previous } { } > ./oprofile_data/opreport/stats.diff
 fi
 
 # Archive
-oparchive -o ./oparchive-previous ./vswitchd/ovs-vswitchd
+oparchive -o ./oprofile_data/oparchive/previous ./vswitchd/ovs-vswitchd
 
 cd ./vswitchd
