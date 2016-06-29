eb14178a468a4b6b7f34c5910a162c0ffc18b7fa
Sean Choi <yo2seol@gmail.com>
2016-06-28 17:28:24 -0700

    Removing custom elements that blocks build

--- include/p4/examples/add_to_field/simple_sub_from_field.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh -ve
-
-DIR=/home/ubuntu/Research/ovs/utilities
-
-$DIR/ovs-ofctl --protocols=OpenFlow15 del-flows br0
-$DIR/ovs-ofctl --protocols=OpenFlow15 add-flow br0 "in_port=1, actions=sub_from_field:2->ethernet__etherType,deparse,output:2"
